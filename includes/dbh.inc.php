3c6be6e44f9b95e9c51e7f7842b75da4bab9cdea
Priyankarajput1 <priyankarajput3010@gmail.com>
2018-08-11 00:40:00 +0530

    first commit

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "127.0.0.1";
+$dbusername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbusername, $dbPassword, $dbName);
+?>
